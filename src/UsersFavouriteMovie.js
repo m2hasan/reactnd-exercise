5c8709ff030a656a275b23f5b13dd69dc0c57bac
Mahbub Hasan <masumm17@yahoo.com>
2020-04-20 20:36:36 +0000

    Completed users favorite movie list fix movies name

--- src/UsersFavouriteMovie.js
+++ src/UsersFavouriteMovie.js
@@ -4,7 +4,7 @@ class UsersFavouriteMovie extends Component {
 	render (){
     	return <ul class='users-favourite-movies-list'>
           {this.props.profiles.map((profile) => (
-          	<li>{`${this.props.users[profile.userID].name}'s favorite movie is ${this.props.users[profile.favoriteMovieID].name}.`}</li>
+          	<li>{`${this.props.users[profile.userID].name}'s favorite movie is ${this.props.movies[profile.favoriteMovieID].name}.`}</li>
           ))}
         </ul>
     }
